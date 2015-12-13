4b7f008c691d14894ea17c3263c7c68fab66f246
ExcaliburZero <cwellsny@nycap.rr.com>
2015-12-12 21:10:06 -0500

    Add basic package directory and init file
    Add a basic package directory containing a simple __init__.py file to define the elements of the package.

---
+++ blackjack/__init__.py
@@ -0,0 +1,7 @@
+"""The init file for the blackjack package."""
+
+__author__ = 'Christopher Randall Wells'
+__copyright__ = 'Copyright 2015 Christopher Randall Wells'
+__license__ = 'MIT'
+__title__ = 'blackjack'
+__version__ = '0.1'
