a2d471ceeba792d133cbe81de85c55d6f02292ab
ExcaliburZero <cwellsny@nycap.rr.com>
2015-12-20 20:32:45 -0500

    Add method to get the next action of a dealer
    Add the get_action method to the Dealer class to be able to get the next action of the dealer based on the cards that they have. Also add testing for the new method.

--- blackjack/dealers.py
+++ blackjack/dealers.py
@@ -1,5 +1,6 @@
 """This module is used to store the classes related to the dealer."""
 from .people import Person
+from .actions import Action
 
 
 class Dealer(Person):
@@ -16,3 +17,25 @@ class Dealer(Person):
         self.name = "Dealer"
         # The cards that the dealer has
         self.cards = []
+
+    def get_action(self):
+        """
+        The method that is used to get the next action of the dealer.
+
+        :returns Action: The next action of the dealer.
+        """
+
+        # See if the dealer's cards are certainly over or equal to 17
+        any_below = False
+        for result in self.card_values():
+            if result < 17:
+                any_below = True
+
+        # Decide on the action based on whether or not all possibilities are over or equal to 17
+        if any_below:
+            next_action = Action("Hit")
+        else:
+            next_action = Action("Stand")
+
+        # Return the action
+        return next_action
--- tests/test_dealer.py
+++ tests/test_dealer.py
@@ -2,6 +2,7 @@
 import unittest
 
 from blackjack import Dealer
+from blackjack import Card
 
 
 class TestDealer(unittest.TestCase):
@@ -20,3 +21,32 @@ class TestDealer(unittest.TestCase):
         # Make sure the dealer does not start with any cards
         dealer = Dealer()
         self.assertEqual(dealer.cards, [])
+
+    def test_get_action(self):
+        """A test which makes sure that the get_action method of the Dealer class functions properly."""
+
+        # Try several test cases
+        test_cases = [
+            [Card("Clubs", "2"), Card("Clubs", "2"), "Hit", ],
+            [Card("Clubs", "10"), Card("Clubs", "10"), "Stand", ],
+            [Card("Clubs", "10"), Card("Clubs", "7"), "Stand", ],
+            [Card("Clubs", "10"), Card("Clubs", "6"), "Hit", ],
+            [Card("Clubs", "Ace"), Card("Clubs", "6"), "Hit", ],
+            [Card("Clubs", "Ace"), Card("Clubs", "6"), Card("Clubs", "10"), "Stand", ],
+            [Card("Clubs", "Jack"), Card("Clubs", "7"), "Stand", ],
+            [Card("Clubs", "Queen"), Card("Clubs", "6"), "Hit", ],
+            [Card("Clubs", "King"), Card("Clubs", "6"), "Hit", ],
+            [Card("Clubs", "Ace"), Card("Clubs", "Ace"), Card("Clubs", "6"), "Hit", ],
+            [Card("Clubs", "Ace"), Card("Clubs", "Ace"), Card("Clubs", "6"), "Hit", ],
+            [Card("Clubs", "7"), Card("Clubs", "8"), "Hit", ],
+            [Card("Clubs", "7"), Card("Clubs", "8"), Card("Clubs", "Ace"), "Hit", ],
+            [Card("Clubs", "7"), Card("Clubs", "8"), Card("Clubs", "2"), "Stand", ],
+            [Card("Clubs", "Jack"), Card("Clubs", "Queen"), "Stand", ],
+        ]
+        for case in test_cases:
+            dealer = Dealer()
+            cards = len(case) - 1
+            for index in range(cards):
+                dealer.add_card(case[index])
+            action = dealer.get_action()
+            self.assertEqual(action.move, case[-1], msg="The test of the get_action method with the index of " + str(test_cases.index(case)) + " failed.")
